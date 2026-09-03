24a6eb38ea693b41a12b93d1a73feb9cf934e2bb
Shivam kumar Choudhary <shivamkumarchoudhary027@gmail.com>
2026-09-03 19:04:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BST Insert/bst-insert.java
@@ -0,0 +1,31 @@
+/* Structure of a Binary Search Tree node
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    public Node insert(Node root, int key) {
+        // code here
+        if(root==null){
+            return new Node(key);
+        }
+        
+        if(key<root.data){
+            root.left = insert(root.left, key);
+        }
+        
+        else if(key>root.data){
+            root.right = insert(root.right,key);
+        }
+        else{
+            return root;
+        }
+        return root;
+    }
+}
