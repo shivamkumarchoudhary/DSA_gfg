8b44a569f4ad5d557994d11acd0a1d5f5a715ab7
Shivam kumar Choudhary <shivamkumarchoudhary027@gmail.com>
2026-09-04 13:44:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BST Insert/bst-insert.java
+++ Difficulty: Easy/BST Insert/bst-insert.java
@@ -12,19 +12,14 @@ class Node {
 class Solution {
     public Node insert(Node root, int key) {
         // code here
-        if(root==null){
-            return new Node(key);
+        if(root == null){
+          return  new Node(key);
         }
-        
         if(key<root.data){
-            root.left = insert(root.left, key);
+           root.left = insert(root.left,key);
         }
-        
         else if(key>root.data){
-            root.right = insert(root.right,key);
-        }
-        else{
-            return root;
+            root.right =insert(root.right,key);
         }
         return root;
     }
